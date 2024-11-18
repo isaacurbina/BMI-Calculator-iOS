6170ebc6b67cc2a52b67c8e87b9003c73a720c9a
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-18 16:12:17 -0600

    129. Use Optionals in Practice and Add the Finishing Touches with Colour Literals (#7)

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -11,14 +11,18 @@ import UIKit
 class ResultViewController: UIViewController {
 	
 	var bmiValue: String?
+	var advice: String?
+	var color: UIColor?
 
 	@IBOutlet private weak var bmiLabel: UILabel!
 	
-	@IBOutlet private weak var adviseLabel: UILabel!
+	@IBOutlet private weak var adviceLabel: UILabel!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		bmiLabel.text = bmiValue
+		adviceLabel.text = advice
+		view.backgroundColor = color
     }
     
 	@IBAction private func recalculatePressed(_ sender: UIButton) {
