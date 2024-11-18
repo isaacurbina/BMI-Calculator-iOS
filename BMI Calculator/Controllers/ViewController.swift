f46e39a2a3a3cf01be4aaae5dafe476b8c139404
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-17 20:08:04 -0600

    125. How to Create a UI Programatically and Pass Data between ViewControllers (#4)

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -41,6 +41,10 @@ class ViewController: UIViewController {
 		let weight = weightSlider.value
 		let bmi = weight / pow(height, 2)
 		print(bmi)
+		
+		let secondVC = SecondViewController()
+		secondVC.bmiValue =  String(format: "%.1f", bmi)
+		self.present(secondVC, animated: true, completion: nil)
 	}
 	
 	// --- MARK: - private functions ---
