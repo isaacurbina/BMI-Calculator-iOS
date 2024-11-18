ecc01785c0f4b2ef927b41d4e8f3177f2d56a2b9
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-18 16:11:32 -0600

    129. Use Optionals in Practice and Add the Finishing Touches with Colour Literals

---
+++ BMI Calculator/.swift
@@ -0,0 +1,3 @@
+enum WeightClass {
+	case underweight, normal, overweight
+}
--- BMI Calculator/CalculatorBrain.swift
+++ BMI Calculator/CalculatorBrain.swift
@@ -3,15 +3,33 @@ import UIKit
 
 struct CalculatorBrain {
 	
-	private var bmi: Float? = nil
+	private var bmi: BMI? = nil
 	
 	mutating func calculateBMI(height: Float, weight: Float) {
-		bmi = weight / pow(height, 2)
+		let bmiValue = weight / pow(height, 2)
+		getBMI(bmiValue)
 	}
 	
 	func getBMIValue() -> String {
-		let bmiValueToOneDecimalPlace = String(format: "%.1f", bmi ?? 0.0)
+		let bmiValueToOneDecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
 		return bmiValueToOneDecimalPlace
 	}
+	
+	func getAdvice() -> String {
+		return bmi?.advice ?? "N/A"
+	}
+	
+	func getColor() -> UIColor {
+		return bmi?.color ?? .black
+	}
+	
+	private mutating func getBMI(_ bmiValue: Float) {
+		if bmiValue < 18.5 {
+			bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+		} else if bmiValue < 24.9 {
+			bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+		} else {
+			bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))
+		}
+	}
 }
-
